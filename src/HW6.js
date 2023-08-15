554c5829ed95a9f5b4b7d0f1e29661838369a9ef
faMokhtari <f.mokhtari602@gmail.com>
2023-08-15 19:55:45 +0330

    Update HW6.js
    Changed the default city name.

--- src/HW6.js
+++ src/HW6.js
@@ -71,4 +71,4 @@ searchForm.addEventListener("submit", handleSubmit);
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-searchCity("New York");
+searchCity("Tehran");
